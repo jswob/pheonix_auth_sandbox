9bbc3762cc4da1832d75eba75875c5a2531a70f3
jacek swoboda <jacswob@gmail.com>
2020-02-21 15:22:30 +0100

    Change create user endpoint to use register_user

--- lib/phoenix_auth_sandbox_web/controllers/user_controller.ex
+++ lib/phoenix_auth_sandbox_web/controllers/user_controller.ex
@@ -7,7 +7,7 @@ defmodule PhoenixAuthSandboxWeb.UserController do
   action_fallback PhoenixAuthSandboxWeb.FallbackController
 
   def create(conn, %{"user" => user_params}) do
-    with {:ok, %User{} = user} <- Accounts.create_user(user_params) do
+    with {:ok, %User{} = user} <- Accounts.register_user(user_params) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", Routes.user_path(conn, :show, user))
