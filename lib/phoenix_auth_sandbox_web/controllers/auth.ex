aa6ed9a26d18173e1a85aea6192dc9b20623d53f
jacek swoboda <jacswob@gmail.com>
2020-02-21 15:20:05 +0100

    Create Auth context for authorization logic

---
+++ lib/phoenix_auth_sandbox_web/controllers/auth.ex
@@ -0,0 +1,48 @@
+defmodule PhoenixAuthSandboxWeb.Auth do
+  import Plug.Conn
+  import Phoenix.Controller
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    user_id = get_session(conn, :user_id)
+
+    require IEx
+    IEx.pry()
+
+    cond do
+      conn.assigns[:current_user] ->
+        conn
+
+      user = user_id && PhoenixAuthSandbox.Accounts.get_user!(user_id) ->
+        assign(conn, :current_user, user)
+
+      true ->
+        assign(conn, :current_user, nil)
+    end
+  end
+
+  def ensure_authenticated(conn, _opts) do
+    if conn.assigns[:current_user] do
+      conn
+    else
+      conn
+      |> put_status(:unauthorized)
+      |> put_view(PhoenixAuthSandboxWeb.ErrorView)
+      |> render("401.json", message: "Unauthenticated user")
+      |> halt()
+    end
+  end
+
+  def login(conn, user) do
+    conn
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
+
+  def logout(conn) do
+    conn
+    |> assign(:current_user, nil)
+    |> configure_session(drop: true)
+  end
+end
